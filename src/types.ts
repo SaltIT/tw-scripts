ec7f3f2340526b834b79bed039a90213abcfaaea
Syreanis <pegak15@seznam.cz>
2020-11-01 15:32:39 +0100

    Add types.ts

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+interface Window {
+  TWMap: { villages: { [key: string]: any } };
+  jQuery: any;
+  UI: any;
+}
+
+interface Object {
+  values: Function;
+}
+
+interface Element {
+  style: { [key: string]: any };
+}
